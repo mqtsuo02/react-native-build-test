5ce49b8b1b9eedd612bc2cebdb95330230608102
mqtsuo02 <obu.tokyo@gmail.com>
2018-04-13 15:49:37 +0900

    add: next.js

--- App/App.js
+++ App/App.js
@@ -1,13 +1,5 @@
 import React, { Component } from "react"
-import { Platform } from "react-native"
 import { View, Text, StyleSheet } from "react-primitives"
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
-})
 
 const styles = StyleSheet.create({
   container: {
@@ -35,7 +27,6 @@ export default class App extends Component<Props> {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     )
   }
